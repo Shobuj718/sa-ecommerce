701e71b707e479bf90aa3de7f824c496b7ac0824
shobuj <shobujsa93@gmail.com>
2019-02-18 22:52:05 +0600

    users table migration added

---
+++ database/migrations/2019_02_18_223542_create_users_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 128);
+            $table->strin('email', 128)->unique();
+            $table->string('phone_number', 32)->unique();
+            $table->string('password', 128);
+            $table->bigInteger('reward_points')->default(0);
+            $table->date('email_varified_at')->nullable();
+            $table->string('email_varification_token', 100)nullable();
+            $table->string('facebook_id', 32)nullable();
+            $table->string('google_id', 32)nullable();
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
